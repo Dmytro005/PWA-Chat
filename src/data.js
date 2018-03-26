5c132a979cb4ecbd7bffcdb899e7f7e9036936ee
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-03-26 09:31:10 +0300

    Display posts and view them detailed

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default {
+  pictures: [
+    {
+      'id': 0,
+      'url': 'https://images.unsplash.com/photo-1508616987956-0cdacd91ac2f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=401e531bcb93d5f6712d91e77cfd4997&auto=format&fit=crop&w=700&q=80',
+      'comment': 'Cirque Du Fer A Cheval, Sixt-Fer-à-Cheval, France',
+      'info': 'Posted by Kevin on Friday'
+    },
+    {
+      'id': 1,
+      'url': 'https://images.unsplash.com/photo-1509565840034-3c385bbe6451?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9d25cc8624712ad81d4f50bb843cc022&auto=format&fit=crop&w=1189&q=80',
+      'comment': 'Aliaska',
+      'info': 'Posted by Charles on Tuesday'
+    },
+    {
+      'id': 2,
+      'url': 'https://images.unsplash.com/photo-1491986926302-149ec463b90a?ixlib=rb-0.3.5&s=14b1898eaa8cdc930a366634bc229a84&auto=format&fit=crop&w=1051&q=80',
+      'comment': 'Aoraki Mount Cook Village, New Zealand',
+      'info': 'Posted by Richard on Monday'
+    },
+    {
+      'id': 3,
+      'url': 'https://images.unsplash.com/photo-1504281134378-be05bbb10111?ixlib=rb-0.3.5&s=d6e445d64370a3ff2a32d84ca0f2da2d&auto=format&fit=crop&w=1050&q=80',
+      'comment': 'Ossana, Italy',
+      'info': 'Posted by Richard on Monday'
+    },
+    {
+      'id': 4,
+      'url': 'https://images.unsplash.com/photo-1492553397175-5f7ec0158903?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f13108c72f87cf399a55260c6596694a&auto=format&fit=crop&w=1050&q=80',
+      'comment': 'Alpes',
+      'info': 'Posted by Richard on Monday'
+    }
+  ]
+}
